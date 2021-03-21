d5f587ead53712a4721bee28e366516c172d40f2
quan <13145710069@163.com>
2021-03-21 16:16:05 +0800

    init git

---
+++ src/main/java/com/better/lecodeall/LecodeAllApplication.java
@@ -0,0 +1,13 @@
+package com.better.lecodeall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecodeAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecodeAllApplication.class, args);
+    }
+
+}
